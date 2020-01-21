a56e7f2d702c7d0b3e44c06091b1bcc2659f6a90
dipuvish5 <39113917+dipuvish5@users.noreply.github.com>
2020-01-21 14:33:38 +0530

    React Router Example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
 import App from "./App";
+import Users from "./Users";
+import Contact from "./Contact";
+import Notfound from "./NotFound";
+import "./index.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="active" to="/users">
+            Users
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact activeClassName="active" to="/contact">
+            Contact
+          </NavLink>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+);
+ReactDOM.render(routing, document.getElementById("root"));
